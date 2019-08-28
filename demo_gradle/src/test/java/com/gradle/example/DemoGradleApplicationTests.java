285a4f1193aa584d93dba982166e94a7fea9b1e1
Rahul Ranjan <rahul.ranjan06@ad.infosys.com>
2019-08-28 13:11:44 +0530

    first Commit

---
+++ demo_gradle/src/test/java/com/gradle/example/DemoGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gradle.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
